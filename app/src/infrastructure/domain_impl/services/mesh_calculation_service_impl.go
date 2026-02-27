213eb0496e57fbceb92a093fea132a3b8da9b084
koichi <111332153+koichi2426@users.noreply.github.com>
2026-02-27 22:42:19 +0900

    ドメインサービスのダミー実装

---
+++ app/src/infrastructure/domain_impl/services/mesh_calculation_service_impl.go
@@ -0,0 +1,18 @@
+package domain_impl_services
+
+import (
+	"src/domain/services"
+	"src/domain/value_objects"
+)
+
+// MeshCalculationServiceImplはMeshCalculationServiceインターフェースの具象実装です。
+type MeshCalculationServiceImpl struct{}
+
+func NewMeshCalculationServiceImpl() services.MeshCalculationService {
+	return &MeshCalculationServiceImpl{}
+}
+
+// CalculateMeshIDは緯度経度からMeshIDを算出します（ダミー実装）。
+func (s *MeshCalculationServiceImpl) CalculateMeshID(lat value_objects.Latitude, lng value_objects.Longitude) (value_objects.MeshID, error) {
+	return value_objects.MeshID("dummy-mesh-id"), nil
+}
