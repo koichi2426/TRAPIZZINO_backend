6b0d3c4392f871d7bc05484cf306b1abbd8b3081
koichi <111332153+koichi2426@users.noreply.github.com>
2026-02-28 00:15:13 +0900

    バリデーション修正

--- app/src/domain/value_objects/email.go
+++ app/src/domain/value_objects/email.go
@@ -7,7 +7,7 @@ import (
 
 type Email string
 
-var emailRegex = regexp.MustCompile(`^[\w._%+-]+@[\w.-]+\\.[a-zA-Z]{2,}$`)
+var emailRegex = regexp.MustCompile(`^[\w._%+-]+@[\w.-]+\.[a-zA-Z]{2,}$`)
 
 func NewEmail(value string) (Email, error) {
 	if !emailRegex.MatchString(value) {
